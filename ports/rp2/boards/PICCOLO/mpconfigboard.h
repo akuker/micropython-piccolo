8a6d7810a20215927cbd8bd4d0e23e1528fbd66f
Tony Kuker <akuker@gmail.com>
2023-03-28 21:12:10 -0500

    modifications to support piccolo

---
+++ ports/rp2/boards/PICCOLO/mpconfigboard.h
@@ -0,0 +1,5 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+
+#define PICO_BOOT_STAGE2_CHOOSE_GENERIC_03H 1
